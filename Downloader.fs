bdb341821799696c9bccd137b5aae25925893f48
Vest <Vest@users.noreply.github.com>
2022-11-10 21:11:37 +0100

    Initial commit (doesn't work/compile)
    Signed-off-by: Vest <Vest@users.noreply.github.com>

---
+++ Downloader.fs
@@ -0,0 +1,15 @@
+﻿module aoc.Downloader
+
+open FSharp.Data
+
+let private adventUrl (year : uint, day : uint) : string = $"https://adventofcode.com/{year}/day/{day}/input"
+let private cookies =
+    try
+        Some(System.Environment.GetEnvironmentVariable "ADVENT_SESSION")
+    with
+        | None
+let inputData year day =
+    match cookies with
+    | Some(cookies) -> Http.RequestString (url = adventUrl (year day), cookies = ["session", cookies])
+    | _ -> ""
+
---
+++ aoc-test/DownloaderTests.fs
@@ -0,0 +1,9 @@
+﻿module aoc_test.DownloaderTests
+
+open aoc.Downloader
+open Xunit
+
+[<Fact>]
+let sampleAocUrl() =
+    let url = aoc.Downloader.adventUrl 2022 7
+    Assert.Equal(url, "https://adventofcode.com/2022/day/7/input")
