1a61b3f7c39375d0cd86b3759109d0ece738b112
Vest <Vest@users.noreply.github.com>
2022-11-20 12:47:06 +0100

    Added input caching functionality

--- InputCaching.fs
+++ InputCaching.fs
@@ -1,8 +1,21 @@
 module aoc.InputCaching
 
+open System.IO
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("aoc-test")>]
 do ()
 
-let internal inputFolder (year: int) (day: int) : string = $"input/{year}/{day,2:D2}/"
+let internal inputFile (year: int) (day: int) : string = $"input/{year}/{day,2:D2}.txt"
+
+let readInput (year: int) (day: int) : string option =
+    let path = inputFile year day
+    match File.Exists path with
+    | true ->
+        try
+            File.ReadAllText path |> Some
+        with e ->
+            eprintfn $"An exception occured, while reading the file '{path}': {e.Message}\n{e.StackTrace}"
+            None
+    | false ->
+        None
--- aoc-test/InputCachingTests.fs
+++ aoc-test/InputCachingTests.fs
@@ -4,6 +4,23 @@ open aoc.InputCaching
 open Xunit
 
 [<Fact>]
-let ``Sample Input formatting`` () =
-    let url = inputFolder 2022 7
-    Assert.Equal("input/2022/07/", url)
+let ``Sample input path`` () =
+    let path = inputFile 2022 7
+    Assert.Equal("input/2022/07.txt", path)
+
+[<Fact>]
+let ``Read non-existing input`` () =
+    let input = readInput 2022 7
+    Assert.Equal(None, input)
+
+[<Fact>]
+let ``Read existing input`` () =
+    try
+        let path = inputFile 2022 7
+        System.IO.Directory.CreateDirectory "input/2022" |> ignore
+        System.IO.File.WriteAllText(path, "test")
+        let input = readInput 2022 7
+
+        Assert.Equal(Some "test", input)
+    finally
+        System.IO.Directory.Delete("input", true)
