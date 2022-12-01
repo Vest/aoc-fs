13edb8ab6b5585576756e26937b5b0d559505a45
Vest <Vest@users.noreply.github.com>
2022-12-01 10:52:23 +0100

    Current Day: 01/2022 The first answer is 70374 The second answer is 204610

---
+++ 2022/Day1.fs
@@ -0,0 +1,51 @@
+module aoc.year2022.Day1
+
+open System
+
+let answer1 (input: string) : int =
+    let answer =
+        let index = ref 0
+
+        input.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
+        |> Array.toSeq
+        |> Seq.map (fun line ->
+            match Int32.TryParse line with
+            | true, out -> Some out
+            | false, _ -> None)
+        |> Seq.map (fun (calories: int option) ->
+            match calories with
+            | Some calories -> (index.Value, calories)
+            | None ->
+                index.Value <- index.Value + 1
+                (index.Value, 0))
+        |> Seq.groupBy fst
+        |> Seq.map snd
+        |> Seq.map (fun elfCalories ->
+            elfCalories |> Seq.map snd |> Seq.sum)
+        |> Seq.max
+    answer
+
+let answer2 (input: string) : int =
+    let answer =
+        let index = ref 0
+
+        input.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
+        |> Array.toSeq
+        |> Seq.map (fun line ->
+            match Int32.TryParse line with
+            | true, out -> Some out
+            | false, _ -> None)
+        |> Seq.map (fun (calories: int option) ->
+            match calories with
+            | Some calories -> (index.Value, calories)
+            | None ->
+                index.Value <- index.Value + 1
+                (index.Value, 0))
+        |> Seq.groupBy fst
+        |> Seq.map snd
+        |> Seq.map (fun elfCalories ->
+            elfCalories |> Seq.map snd |> Seq.sum)
+        |> Seq.sortDescending
+        |> Seq.take 3
+        |> Seq.sum
+    answer
--- Program.fs
+++ Program.fs
@@ -1,8 +1,26 @@
-﻿open aoc.Input
+﻿open aoc.year2022
+open aoc.Input
+open System
 
 printfn "Hello from aoc-fs"
 
-let input = readDay 2021 6
-match input with
-| Some input -> printfn $"Read input: '{input}'"
-| None -> printfn "Nothing was found :("
+
+
+// let input = readDay 2022 1
+// match input with
+// | Some input -> printfn $"Read input: '{input}'"
+// | None -> printfn "Nothing was found :("
+
+[<EntryPoint>]
+let main(args) =
+    let year = args[0] |> int
+    let day = args[1]  |> int
+    printfn $"Current Day: {day, 2:D2}/{year}"
+
+    let input = readDay year day
+    match input with
+    | Some input ->
+        printfn $"The first answer is {Day1.answer1 input}"
+        printfn $"The second answer is {Day1.answer2 input}"
+    | None -> eprintfn "Sorry, but there is no input for the current day"
+    0
