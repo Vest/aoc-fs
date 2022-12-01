48dd29444b70e3d612046f52647e42d9fd42b861
Vest <Vest@users.noreply.github.com>
2022-12-01 11:41:38 +0100

    Small refactoring

--- Program.fs
+++ Program.fs
@@ -1,30 +1,32 @@
 ﻿open aoc.year2022
 open aoc.Input
 
+let displayAnswer (answer: string) (isError: bool) : unit =
+    match isError with
+    | true -> printfn $"{answer}"
+    | false -> eprintfn $"{answer}"
+
+let display year day =
+    let input = readDay year day
+
+    let answer =
+        match input, day with
+        | Some input, 1 -> (false, $"Day {day} / {year}: {Day1.answer1 input} and {Day1.answer2 input}")
+        | _ -> (true, $"Day {day} / {year}: Sorry, but there is no input for the current day")
+
+    displayAnswer (snd answer) (fst answer)
+
 [<EntryPoint>]
 let main args =
     if args.Length = 2 then
         let year = args[0] |> int
         let day = args[1] |> int
 
-        let input = readDay year day
-
-        match input with
-        | Some input -> printfn $"Day {day} / {year}: {Day1.answer1 input} and {Day1.answer2 input}"
-        | None -> eprintfn $"Day {day} / {year}: Sorry, but there is no input for the current day"
+        display year day
     else
         seq { 1..25 }
         |> Seq.iter (fun day ->
             let year = 2022
-            let input = readDay year day
-
-            let answer =
-                match input, day with
-                | Some input, 1 -> $"{Day1.answer1 input} and {Day1.answer2 input}"
-                | _ -> "Sorry, but there is no input for the current day"
-
-            match input with
-            | Some input -> printfn $"Day {day} / {year}: {answer}"
-            | None -> eprintfn $"Day {day} / {year}: {answer}")
+            display year day)
 
     0
