a048e25bec7de8c1e767ca0f225ee4e39dee3d60
Vest <Vest@users.noreply.github.com>
2022-12-01 10:54:55 +0100

    Day 1 / 2022: 70374 and 204610

--- Program.fs
+++ Program.fs
@@ -1,26 +1,15 @@
 ﻿open aoc.year2022
 open aoc.Input
-open System
-
-printfn "Hello from aoc-fs"
-
-
-
-// let input = readDay 2022 1
-// match input with
-// | Some input -> printfn $"Read input: '{input}'"
-// | None -> printfn "Nothing was found :("
 
 [<EntryPoint>]
-let main(args) =
+let main (args) =
     let year = args[0] |> int
-    let day = args[1]  |> int
-    printfn $"Current Day: {day, 2:D2}/{year}"
+    let day = args[1] |> int
 
     let input = readDay year day
+
     match input with
-    | Some input ->
-        printfn $"The first answer is {Day1.answer1 input}"
-        printfn $"The second answer is {Day1.answer2 input}"
+    | Some input -> printfn $"Day {day} / {year}: {Day1.answer1 input} and {Day1.answer2 input}"
     | None -> eprintfn "Sorry, but there is no input for the current day"
+
     0
