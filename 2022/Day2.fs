57792b6c0d70b324ec62b1121fb623a1ac7b89c1
Vest <Vest@users.noreply.github.com>
2022-12-02 16:42:44 +0100

    Day 2 / 2022: Added tests, solved the sample input

--- 2022/Day2.fs
+++ 2022/Day2.fs
@@ -1,13 +1,14 @@
 module aoc.year2022.Day2
 
+open System
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("aoc-test")>]
 do ()
 
 type Winner =
-    | Win
-    | Lost
+    | PlayerWon
+    | PlayerLost
     | Draw
 
 type Choice =
@@ -15,31 +16,61 @@ type Choice =
     | Paper
     | Scissors
 
-let internal playRound (elf :Choice) (player : Choice) : Winner =
+let internal playRound (elf: Choice) (player: Choice) : Winner =
     match (elf, player) with
     | (ch1, ch2) when ch1 = ch2 -> Draw
-    | (Rock, Paper) -> Win
-    | (Paper, Scissors) -> Win
-    | (Scissors, Rock) -> Win
-    | _ -> Lost
+    | (Rock, Paper) -> PlayerWon
+    | (Paper, Scissors) -> PlayerWon
+    | (Scissors, Rock) -> PlayerWon
+    | _ -> PlayerLost
 
 let internal parseChoice c =
     match c with
-    | 'A' | 'X' -> Some Rock
-    | 'B' | 'Y' -> Some Paper
-    | 'C' | 'Z' -> Some Scissors
+    | 'A'
+    | 'X' -> Some Rock
+    | 'B'
+    | 'Y' -> Some Paper
+    | 'C'
+    | 'Z' -> Some Scissors
     | _ -> None
 
-let internal parse (input : string) : (Choice * Choice) option =
+let internal score round choice =
+    let r =
+        match round with
+        | PlayerWon -> 6
+        | PlayerLost -> 0
+        | Draw -> 3
+
+    let c =
+        match choice with
+        | Rock -> 1
+        | Paper -> 2
+        | Scissors -> 3
+
+    r + c
+
+let internal parse (input: string) : (Choice * Choice) option =
     if input.Length = 3 then
         let letter1 = input[0]
         let letter2 = input[2]
+
         match (parseChoice letter1, parseChoice letter2) with
-        | (Some ch1, Some ch2) -> Some (ch1, ch2)
+        | (Some ch1, Some ch2) -> Some(ch1, ch2)
         | _ -> None
     else
         None
 
-let answer1 (input: string) : int = 0
+let answer1 (input: string) : int =
+    input.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
+    |> Array.toSeq
+    |> Seq.map (fun line -> parse line)
+    |> Seq.map (fun round ->
+        match round with
+        | Some(elf, player) ->
+            let round = playRound elf player
+            let score = score round player
+            score
+        | None -> 0)
+    |> Seq.sum
 
 let answer2 (input: string) : int = 0
--- aoc-test/2022/Day2Tests.fs
+++ aoc-test/2022/Day2Tests.fs
@@ -8,3 +8,10 @@ let ``Parse Input`` () =
     Assert.Equal(Some (Rock, Paper), parse "A Y")
     Assert.Equal(Some (Paper, Rock), parse "B X")
     Assert.Equal(Some (Scissors, Scissors), parse "C Z")
+    Assert.Equal(None, parse "C W")
+    Assert.Equal(None, parse "XY")
+    Assert.Equal(None, parse "XYZAB")
+
+[<Fact>]
+let ``First Answer`` () =
+    Assert.Equal(15, answer1 "A Y\nB X\nC Z")
