f4a347c07862e7db39a336eac9a850bf8ca70c2f
Vest <Vest@users.noreply.github.com>
2022-12-08 17:03:13 +0100

    Day 8 / 2022: 1679

--- 2022/Day8.fs
+++ 2022/Day8.fs
@@ -9,17 +9,38 @@ do ()
 type Field =
     { Tree: int[][] }
 
-    member this.isVisible(row: int, col: int) : bool =
+    member this.isVisible (row: int) (col: int) : bool =
+        let width = this.Tree[0].Length
+        let height = this.Tree.Length
+
         match (row, col) with
         | 0, _ -> true
         | _, 0 -> true
-        | r, _ when r = this.Tree.Length - 1 -> true
-        | _, c when c = this.Tree[0].Length - 1 -> true
+        | r, _ when r = height - 1 -> true
+        | _, c when c = width - 1 -> true
         | _, _ ->
             // up
-            [ row - 1 .. 0 ]
-            |> List.tryFind (fun r -> this.Tree[r][col] >= this.Tree[r + 1][col])
-            |> Option.isNone
+            let up =
+                [ 0 .. row - 1 ]
+                |> List.tryFind (fun r -> this.Tree[r][col] >= this.Tree[row][col])
+                |> Option.isNone
+
+            let down =
+                [ row + 1 .. height - 1 ]
+                |> List.tryFind (fun r -> this.Tree[r][col] >= this.Tree[row][col])
+                |> Option.isNone
+
+            let left =
+                [ 0 .. col - 1 ]
+                |> List.tryFind (fun c -> this.Tree[row][c] >= this.Tree[row][col])
+                |> Option.isNone
+
+            let right =
+                [ col + 1 .. width - 1 ]
+                |> List.tryFind (fun c -> this.Tree[row][c] >= this.Tree[row][col])
+                |> Option.isNone
+
+            up || down || left || right
 
 let internal parseLine (line: string) : int[] =
     let inline charToInt c = int c - int '0'
@@ -32,6 +53,14 @@ let internal parseForest (input: string) : int[][] =
 
 let internal createField input = { Tree = parseForest input }
 
-let answer1 (input: string) : int = 0
+let answer1 (input: string) : int =
+    let field = createField input
+    let width = field.Tree[0].Length
+    let height = field.Tree.Length
+
+    [0..height - 1]
+    |> List.allPairs [0..width - 1]
+    |> List.filter (fun (row, col) -> field.isVisible row col)
+    |> List.length
 
 let answer2 (input: string) : int = 0
--- aoc-test/2022/Day8Tests.fs
+++ aoc-test/2022/Day8Tests.fs
@@ -34,7 +34,25 @@ let ``Is visible: edge`` () =
 
     [ 0..4 ]
     |> List.iter (fun i ->
-        Assert.True(forest.isVisible (0, i))
-        Assert.True(forest.isVisible (4, i))
-        Assert.True(forest.isVisible (i, 0))
-        Assert.True(forest.isVisible (i, 4)))
+        Assert.True(forest.isVisible 0 i)
+        Assert.True(forest.isVisible 4 i)
+        Assert.True(forest.isVisible i 0)
+        Assert.True(forest.isVisible i 4))
+
+[<Fact>]
+let ``Is visible - sample field`` () =
+    let forest: Field = createField input
+
+    Assert.True(forest.isVisible 1 1)
+    Assert.True(forest.isVisible 1 2)
+    Assert.False(forest.isVisible 1 3)
+    Assert.True(forest.isVisible 2 1)
+    Assert.False(forest.isVisible 2 2)
+    Assert.True(forest.isVisible 2 3)
+    Assert.False(forest.isVisible 3 1)
+    Assert.True(forest.isVisible 3 2)
+    Assert.False(forest.isVisible 3 3)
+
+[<Fact>]
+let ``First Answer`` () =
+    Assert.Equal(21, answer1 input)
