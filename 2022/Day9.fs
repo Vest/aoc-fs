66cc5f4bdb268a5fc614848f1b230f60c4e0a576
Vest <Vest@users.noreply.github.com>
2022-12-09 22:35:33 +0100

    Day 9 / 2022: Initial commit

---
+++ 2022/Day9.fs
@@ -0,0 +1,88 @@
+module aoc.year2022.Day9
+
+open System
+open System.Runtime.CompilerServices
+
+[<assembly: InternalsVisibleTo("aoc-test")>]
+do ()
+
+type Coord = { row: int; col: int }
+
+type Rope = { head: Coord; tail: Coord }
+
+let internal moveRight coord =
+    { row = coord.row; col = coord.col + 1 }
+
+let internal moveLeft coord =
+    { row = coord.row; col = coord.col - 1 }
+
+let internal moveUp coord =
+    { row = coord.row - 1; col = coord.col }
+
+let internal moveDown coord =
+    { row = coord.row + 1; col = coord.col }
+
+let internal updateTail rope =
+    match rope with
+    | { head = head; tail = tail } when head = tail -> rope
+    | { head = head; tail = tail } when abs (head.row - tail.row) <= 1 && abs (head.col - tail.col) <= 1 -> rope
+    | { head = head; tail = tail } when head.row = tail.row || head.col = tail.col ->
+        { head = head
+          tail =
+            { row = (head.row + tail.row) / 2
+              col = (head.col + tail.col) / 2 } }
+    | { head = head; tail = tail } when abs (head.col - tail.col) = 1 ->
+        { head = head
+          tail =
+            { row = (head.row + tail.row) / 2
+              col = head.col } }
+    | { head = head; tail = tail } when abs (head.row - tail.row) = 1 ->
+        { head = head
+          tail =
+            { row = head.row
+              col = (head.col + tail.col) / 2 } }
+    | _ -> failwith "couldn't happen?!"
+
+type Movement =
+    | Right of int
+    | Left of int
+    | Up of int
+    | Down of int
+
+let internal parseLine (input: string) : Movement =
+    let steps = input.Substring 2 |> Int32.Parse
+
+    match input with
+    | i when i[0] = 'R' -> Right steps
+    | i when i[0] = 'L' -> Left steps
+    | i when i[0] = 'U' -> Up steps
+    | i when i[0] = 'D' -> Down steps
+    | _ -> failwith "Wrong input, unexpected string"
+
+let answer1 (input: string) : int =
+    let rope: Rope =
+        { head = { row = 0; col = 0 }
+          tail = { row = 0; col = 0 } }
+
+    let len =
+        input.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
+        |> Array.toSeq
+        |> Seq.map parseLine
+        |> Seq.fold
+            (fun rope movement ->
+                // helper function
+                let updateRope (move: Coord -> Coord) (rope: Rope) : Rope =
+                    let newHead = move rope.head
+                    let newRope = { head = newHead; tail = rope.tail }
+                    updateTail newRope
+
+                match movement with
+                | Right steps -> [ 1..steps ] |> List.fold (fun rope _ -> updateRope moveRight rope) rope
+                | Left steps -> [ 1..steps ] |> List.fold (fun rope _ -> updateRope moveLeft rope) rope
+                | Up steps -> [ 1..steps ] |> List.fold (fun rope _ -> updateRope moveUp rope) rope
+                | Down steps -> [ 1..steps ] |> List.fold (fun rope _ -> updateRope moveDown rope) rope)
+            rope
+
+    0
+
+let answer2 (input: string) : int = 0
---
+++ aoc-test/2022/Day9Tests.fs
@@ -0,0 +1,118 @@
+module aoc_test.year2022.Day9Tests
+
+open System
+open aoc.year2022.Day9
+open Xunit
+
+let input: string =
+    @"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"
+
+[<Fact>]
+let ``Test movements: Start-Stop`` () =
+    Assert.Equal(
+        { head = { row = 0; col = 0 }
+          tail = { row = 0; col = 0 } },
+        updateTail
+            { head = { row = 0; col = 0 }
+              tail = { row = 0; col = 0 } }
+    )
+
+    Assert.Equal(
+        { head = { row = 2; col = 2 }
+          tail = { row = 2; col = 2 } },
+        updateTail
+            { head = { row = 2; col = 2 }
+              tail = { row = 2; col = 2 } }
+    )
+
+[<Fact>]
+let ``Test movements: Move right`` () =
+    Assert.Equal(
+        { head = { row = 0; col = 2 }
+          tail = { row = 0; col = 1 } },
+        updateTail
+            { head = { row = 0; col = 2 }
+              tail = { row = 0; col = 0 } }
+    )
+
+[<Fact>]
+let ``Test movements: Move left`` () =
+    Assert.Equal(
+        { head = { row = 0; col = 0 }
+          tail = { row = 0; col = 1 } },
+        updateTail
+            { head = { row = 0; col = 0 }
+              tail = { row = 0; col = 2 } }
+    )
+
+
+[<Fact>]
+let ``Test movements: Move up`` () =
+    Assert.Equal(
+        { head = { row = 2; col = 0 }
+          tail = { row = 1; col = 0 } },
+        updateTail
+            { head = { row = 2; col = 0 }
+              tail = { row = 0; col = 0 } }
+    )
+
+[<Fact>]
+let ``Test movements: Move down`` () =
+    Assert.Equal(
+        { head = { row = 2; col = 0 }
+          tail = { row = 1; col = 0 } },
+        updateTail
+            { head = { row = 2; col = 0 }
+              tail = { row = 0; col = 0 } }
+    )
+
+[<Fact>]
+let ``Test movements: Don't move`` () =
+    Assert.Equal(
+        { head = { row = 2; col = 0 }
+          tail = { row = 1; col = 0 } },
+        updateTail
+            { head = { row = 2; col = 0 }
+              tail = { row = 1; col = 0 } }
+    )
+
+    Assert.Equal(
+        { head = { row = 2; col = 2 }
+          tail = { row = 1; col = 1 } },
+        updateTail
+            { head = { row = 2; col = 2 }
+              tail = { row = 1; col = 1 } }
+    )
+
+[<Fact>]
+let ``Test movements: Move diagonally`` () =
+    Assert.Equal(
+        { head = { row = 1; col = 2 }
+          tail = { row = 2; col = 2 } },
+        updateTail
+            { head = { row = 1; col = 2 }
+              tail = { row = 3; col = 1 } }
+    )
+
+    Assert.Equal(
+        { head = { row = 2; col = 3 }
+          tail = { row = 2; col = 2 } },
+        updateTail
+            { head = { row = 2; col = 3 }
+              tail = { row = 3; col = 1 } }
+    )
+
+[<Fact>]
+let ``Parse line`` () =
+    Assert.Equal(Right 10, parseLine "R 10")
+    Assert.Equal(Left 10, parseLine "L 10")
+
+[<Fact>]
+let ``First Answer`` () = Assert.Equal(13, answer1 input)
