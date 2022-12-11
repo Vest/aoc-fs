de92bf27a2a8aa06204db05ff8f86eeef12ff1cf
Vest <Vest@users.noreply.github.com>
2022-12-11 13:30:32 +0100

    Day 10 / 2022: Initial commit

---
+++ 2022/Day10.fs
@@ -0,0 +1,11 @@
+module aoc.year2022.Day10
+
+open System
+open System.Runtime.CompilerServices
+
+[<assembly: InternalsVisibleTo("aoc-test")>]
+do ()
+
+let answer1 (input: string) : int = 0
+
+let answer2 (input: string) : int = 0
---
+++ aoc-test/2022/Day10Tests.fs
@@ -0,0 +1,18 @@
+module aoc_test.year2022.Day10Tests
+
+open System
+open aoc.year2022.Day10
+open Xunit
+
+let input: string =
+    @"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"
+
+[<Fact>]
+let ``Initial`` () = Assert.Equal(0, 0)
