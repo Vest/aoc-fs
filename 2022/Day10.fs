f7e41b77317de8347748a6898d916fa1dc5838bd
Vest <Vest@users.noreply.github.com>
2022-12-11 15:12:39 +0100

    Day 10 / 2022: 15140

--- 2022/Day10.fs
+++ 2022/Day10.fs
@@ -6,6 +6,53 @@ open System.Runtime.CompilerServices
 [<assembly: InternalsVisibleTo("aoc-test")>]
 do ()
 
-let answer1 (input: string) : int = 0
+type Instruction =
+    | NOOP
+    | ADDX of x: int
+
+type CPU = { x: int }
+
+let internal parseLine (line: string) : Instruction =
+    match line with
+    | noop when line.Equals "noop" -> NOOP
+    | addx when line.StartsWith "addx " ->
+        let x = addx.Substring 5 |> Int32.Parse
+        ADDX x
+    | _ -> failwith $"Unknown instruction: {line}"
+
+
+let internal executeInstruction (tick: int -> CPU -> unit) (acc: int) (cpu: CPU) (inst: Instruction) : int * CPU =
+    let acc = acc + 1
+
+    match inst with
+    | NOOP ->
+        tick acc cpu
+        acc, cpu
+    | ADDX x ->
+        tick acc cpu
+        tick (acc + 1) cpu
+        let newCpu = { x = cpu.x + x }
+        (acc + 1), newCpu
+
+let answer1 (input: string) : int =
+    let mutable answer = List.empty
+
+    input.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
+    |> Array.toSeq
+    |> Seq.map parseLine
+    |> Seq.fold
+        (fun (cycle, cpu) instr ->
+            executeInstruction
+                (fun cycle cpu ->
+                    match cycle with
+                    | c when (c - 20) % 40 = 0 -> answer <- cycle * cpu.x :: answer
+                    | _ -> ())
+                cycle
+                cpu
+                instr)
+        (0, { x = 1 })
+    |> ignore
+
+    answer |> List.sum
 
 let answer2 (input: string) : int = 0
--- aoc-test/2022/Day10Tests.fs
+++ aoc-test/2022/Day10Tests.fs
@@ -5,14 +5,184 @@ open aoc.year2022.Day10
 open Xunit
 
 let input: string =
-    @"R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2"
+    @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"
 
 [<Fact>]
-let ``Initial`` () = Assert.Equal(0, 0)
+let ``Parse line`` () =
+    Assert.Equal(NOOP, parseLine "noop")
+    Assert.Equal(ADDX 5, parseLine "addx 5")
+    Assert.Equal(ADDX -50, parseLine "addx -50")
+
+
+[<Fact>]
+let ``CPU: noop`` () =
+    let cpu = { x = 0 }
+    let mutable currentCycle = 0
+
+    let newCpu =
+        NOOP |> executeInstruction (fun cycle _ -> currentCycle <- cycle) 0 cpu |> snd
+
+    Assert.Equal(cpu, newCpu)
+    Assert.Equal(1, currentCycle)
+
+[<Fact>]
+let ``CPU: addx`` () =
+    let cpu = { x = 0 }
+    let mutable currentCycle = 0
+
+    let newCpu =
+        ADDX 5 |> executeInstruction (fun cycle _ -> currentCycle <- cycle) 0 cpu |> snd
+
+    Assert.Equal({ x = 5 }, newCpu)
+    Assert.Equal(2, currentCycle)
+
+
+[<Fact>]
+let ``First Answer`` () =
+    let answer = answer1 input
+    Assert.Equal(13140, answer)
