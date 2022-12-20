f50360c19675312ca73685187460931a0e05b8ba
Vest <Vest@users.noreply.github.com>
2022-12-20 14:25:17 +0100

    Day 12 / 2022: Fixed neighbours search

--- 2022/Day12.fs
+++ 2022/Day12.fs
@@ -12,5 +12,44 @@ let internal parseLandToArray (input: string) : char[][] =
     let lines = input.Split([| "\r\n"; "\r"; "\n" |], StringSplitOptions.None)
     lines |> Array.map (fun line -> line.ToCharArray())
 
+(*let internal createMapOfLand (map: char[][]) : Map<Coord, Coord list> =
+    let height = map |> Array.length
+    let width = map[0] |> Array.length
+
+    ([ 0..height ], [ 0..width ])
+    ||> List.allPairs
+    |> List.map (fun row col -> { row = row; col = col })
+    |> List.map (fun coord ->
+        match coord with
+        | coord when row = 0 && col = 0 -> [ () ])
+    |> ignore
+
+    [| [||] |]*)
+
+
+let internal canMove (fromC: char) (toC: char) : bool =
+    (Convert.ToInt32 toC - Convert.ToInt32 fromC = 1)
+    || (Char.IsLower toC && fromC >= toC)
+    || (toC = 'S')
+    || (fromC = 'z' && toC = 'E')
+    || (fromC = 'S' && toC = 'a')
+
+let internal gatherNeighbours (map: char[][]) (coord: Coord) : (Set<Coord>) =
+    let fromChar: char = map[coord.row][coord.col]
+    let height = map |> Array.length
+    let width = map[0] |> Array.length
+
+    let up = (coord.row - 1, coord.col)
+    let down = (coord.row + 1, coord.col)
+    let left = (coord.row, coord.col - 1)
+    let right = (coord.row, coord.col + 1)
+
+    [ up; down; left; right ]
+    |> List.filter (fun (r, c) -> r >= 0 && r < height && c >= 0 && c < width)
+    |> List.map (fun c -> (c, map[fst c][snd c]))
+    |> List.filter (fun (_, toChar) -> canMove fromChar toChar)
+    |> List.map (fun ((row, col), _) -> { row = row; col = col })
+    |> Set.ofList
+
 let answer1 input = 0
 let answer2 input = 0
--- aoc-test/2022/Day12Tests.fs
+++ aoc-test/2022/Day12Tests.fs
@@ -3,7 +3,39 @@ module aoc_test.year2022.Day12Tests
 open aoc.year2022.Day12
 open Xunit
 
+let input: string =
+    @"Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"
+
 [<Fact>]
 let ``Parse land to array`` () =
     let input: string = "abc\ncde"
-    Assert.Equal<char[]>([| [| 'a'; 'b'; 'c' |]; [| 'c'; 'd'; 'e' |] |], parseLandToArray input)
+    let output = parseLandToArray input
+    Assert.Equal<char[]>([| [| 'a'; 'b'; 'c' |]; [| 'c'; 'd'; 'e' |] |], output)
+    Assert.Equal<char>([| 'a'; 'b'; 'c' |], output[0])
+    Assert.Equal<char>('c', output[0][2])
+
+[<Fact>]
+let ``Can move`` () =
+    Assert.True(canMove 'a' 'a')
+    Assert.True(canMove 'a' 'b')
+    Assert.False(canMove 'a' 'c')
+    Assert.True(canMove 'b' 'a')
+    Assert.True(canMove 'c' 'a')
+    Assert.True(canMove 'z' 'E')
+    Assert.False(canMove 'y' 'E')
+    Assert.True(canMove 'a' 'S')
+    Assert.True(canMove 'b' 'S')
+    Assert.True(canMove 'S' 'a')
+
+[<Fact>]
+let ``Find neighbours`` () =
+    let input = parseLandToArray input
+
+    Assert.Equal<Coord>(
+        Set.empty.Add({ row = 1; col = 0 }).Add({ row = 0; col = 1 }),
+        gatherNeighbours input { row = 0; col = 0 }
+    )
